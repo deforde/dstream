ce8756c6064e1c24dab20e07c61da8047de83bde
deforde <daniel.forde001@gmail.com>
2023-01-12 16:18:57 +0200

    update config test

--- test/src/test_config.c
+++ test/src/test_config.c
@@ -1,5 +1,7 @@
 #include "test_config.h"
 
+#include <unity.h>
+
 #include "config.h"
 
 #define CFG_CONTENT                                                            \
@@ -31,5 +33,27 @@
 
 void testConfigBasic(void) {
     config_t cfg = configParse(CFG_CONTENT);
+
+    TEST_ASSERT_EQUAL_STRING("/path/to/exe", cfg.app);
+
+    TEST_ASSERT_EQUAL_STRING("arg1", cfg.args[0]);
+    TEST_ASSERT_EQUAL_STRING("arg2", cfg.args[1]);
+
+    TEST_ASSERT_EQUAL_STRING("x", cfg.globals[0]);
+    TEST_ASSERT_EQUAL_STRING("s", cfg.globals[1]);
+
+    TEST_ASSERT_EQUAL_STRING("y", cfg.locals[0]->ids[0]);
+    TEST_ASSERT_EQUAL_STRING("p", cfg.locals[0]->ids[1]);
+    TEST_ASSERT_EQUAL_STRING("main.c:38", cfg.locals[0]->loc);
+
+    TEST_ASSERT_EQUAL_STRING("arr", cfg.locals[1]->ids[0]);
+    TEST_ASSERT_EQUAL_STRING("main.c:44", cfg.locals[1]->loc);
+
+    TEST_ASSERT_EQUAL_STRING("data2@len", cfg.locals[2]->ids[0]);
+    TEST_ASSERT_EQUAL_STRING("main.c:21", cfg.locals[2]->loc);
+
+    TEST_ASSERT_EQUAL_STRING("w", cfg.statics[0]->ids[0]);
+    TEST_ASSERT_EQUAL_STRING("main.c", cfg.statics[0]->file);
+
     configDestroy(&cfg);
 }
